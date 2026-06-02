01acc3fc75a2251bdf148a27371a89a8b6c26143
vitor <vitrdeoliveiramattos.99@gmail.com>
2026-06-02 08:43:57 -0300

    b1.c feito

---
+++ listas/lista01/b1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    char nome[50];
+    float nota;
+} Alunos;
+
+int main(){
+
+    int n;
+    float total;
+    float  media;
+    int aprovados;
+
+    printf("Quantos alunos tem na classe?\n");
+    scanf("%d", &n);
+
+    Alunos alunos[n];
+
+    for (int i = 0; i < n; i ++){
+        printf("Qual o nome do aluno?\n");
+        scanf("%s", alunos[i].nome);
+
+        printf("Qual a nota dele?\n");
+        scanf("%f", &alunos[i].nota);
+
+        total = total + alunos[i].nota;
+        if(alunos[i].nota >= 7.0){
+            aprovados++;
+        }
+    }
+
+   media = total / n;
+
+   printf("Tabela  da turma:\n");
+   printf("A media da turma foi: %.2f\n", media);
+   printf("Quantidade de aprovados: %d\n", aprovados);
+}
