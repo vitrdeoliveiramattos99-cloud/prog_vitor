18005cdbdc05cc8fea1ad8bf28aa816568fe7db1
vitrdeoliveiramattos99-cloud <vitrdeoliveiramattos.99@gmail.com>
2026-06-06 15:04:43 -0300

    arrumando b1.c

--- listas/lista01/b1.c
+++ listas/lista01/b1.c
@@ -1,55 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
-// Defini um tamanho fixo máximo para o vetor que o compilador aceite, pq ele não estava acietando
-#define MAX_ALUNOS 100
-
 typedef struct {
     char nome[50];
     float nota;
-} Aluno;
+} Alunos;
 
 int main() {
     int n;
-    float soma = 0.0;
-    int aprovados = 0;
+    float total = 0;
     float media;
+    int aprovados = 0;
 
-    printf("Quantos alunos tem na turma? (Maximo %d):\n", MAX_ALUNOS);
+    printf("Quantos alunos tem na classe? ");
     scanf("%d", &n);
 
-    // SOLUÇÃO DO ERRO: Agora o compilador sabe o tamanho exato na compilação
-    Aluno turma[MAX_ALUNOS];
+    Alunos alunos[n];
 
     for (int i = 0; i < n; i++) {
-        printf("Qual o nome do aluno %d?\n", i++);
-        scanf("%s", turma[i].nome);
+        printf("\nAluno %d\n", i + 1);
 
-        printf("Qual a nota dele?\n", i++);
-        scanf("%f", &turma[i].nota);
-        
-        soma = soma + turma[i].nota;
-        
-        if (turma[i].nota >= 7.0) {
+        printf("Nome: ");
+        scanf("%49s", alunos[i].nome);
+
+        printf("Nota: ");
+        scanf("%f", &alunos[i].nota);
+
+        total += alunos[i].nota;
+
+        if (alunos[i].nota >= 7.0) {
             aprovados++;
         }
-    } 
-
-    media = soma / n;
-
-    printf("\n\n================================");
-    printf("\n       TABELA DE ALUNOS         ");
-    printf("\n================================\n");
-    printf("%-20s | %s\n", "Nome", "Nota");
-    printf("--------------------------------\n");
-    
-    for (int i = 0; i < n; i++) {
-        printf("%-20s | %.2f\n", turma[i].nome, turma[i].nota);
     }
+
+    media = total / n;
+
+    printf("\n================================\n");
+    printf("         TABELA DA TURMA\n");
+    printf("================================\n");
+    printf("%-20s %s\n", "Nome", "Nota");
     printf("--------------------------------\n");
 
+    for (int i = 0; i < n; i++) {
+        printf("%-20s %.2f\n", alunos[i].nome, alunos[i].nota);
+    }
+
+    printf("================================\n");
     printf("Media da turma: %.2f\n", media);
-    printf("Quantidade de aprovados: %d\n", aprovados);
+    printf("Aprovados: %d\n", aprovados);
 
     return 0;
 }
